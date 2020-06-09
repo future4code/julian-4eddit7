ef1bab7d4cbd7b12219049f3d6c79c4e5e562d4c
maxwelb-assis <max.assis@ymail.com>
2020-06-09 14:18:14 -0300

    api e local storage

--- labeddit-7/src/components/FeedPage/index.js
+++ labeddit-7/src/components/FeedPage/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import FormItem from '../FormItem';
 import CardPost from '../CardPost';
+import { useHistory } from "react-router-dom"
+
+
+
 
 function FeedPage() {
+  const history = useHistory()
+
+  useEffect(() => {
+
+    const token = localStorage.getItem("token");
+
+    if (token === null) {
+      history.push("/login");
+    }
+  }, [history]);
+
+
   return (
     <div>
 
